f36bc5082b931bea33490588b75846b24bf97a7c
Quan Dang <danganhquan246@gmail.com>
2023-12-28 21:19:59 +0700

    Yolov5-tensorrtx and voc dataset

--- tensorrt_demo/jetson_nano-master/fix_ssl_certificate.sh
+++
@@ -1,5 +0,0 @@
-# I found this fix from the following JetsonHacks' script
-# https://github.com/jetsonhacks/installROSTX1/blob/master/installROS.sh
-
-# This fixes the issue of wget failing with "ERROR: cannot verify XXXXX's certificate"
-sudo c_rehash /etc/ssl/certs
